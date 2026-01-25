35a36c5ab92f8b74b4068f95d33353739330b1c4
Ignacio Tampe (Chopo) <49919240+igtampe@users.noreply.github.com>
2026-01-25 05:21:08 -0500

    Include IANA timezone

--- OpenMeteo.Common/OpenMeteoResponse.cs
+++ OpenMeteo.Common/OpenMeteoResponse.cs
@@ -1,6 +1,8 @@
 ﻿namespace OpenMeteo.Common {
     public class OpenMeteoResponse {
         
+        public string Timezone { get; set; } = "";
+
         public OpenMeteoCurrent Current { get; set; } = default!;
         public OpenMeteoDaily Daily { get; set; } = default!;
     }
